6646c996ade81890b96477c89976cfca6378584a
maronnax <maronnax@e2b6e7e4-f0f2-0310-8dec-eb45598a66c7>
2008-06-24 20:30:14 +0000

    Foo.
    git-svn-id: https://moleculizer.svn.sourceforge.net/svnroot/moleculizer/branches/libmoleculizer@456 e2b6e7e4-f0f2-0310-8dec-eb45598a66c7

---
+++ src/template/tests/mzrTest.cpp
@@ -0,0 +1,29 @@
+#include <boost/test/included/unit_test.hpp>
+using namespace boost::unit_test;
+
+
+void test_function()
+{
+    throw 0;
+    BOOST_CHECK( 0 == 0); // non-critical test => continue after failure
+}
+
+
+//____________________________________________________________________________//
+
+
+test_suite*
+init_unit_test_suite( int, char* [] ) {
+    framework::master_test_suite().p_name.value = "MzrUnit_Test";
+
+    // register the test case in test tree and specify number of expected failures so
+    // this example will pass at runtime. We expect 2 errors: one from failed check and 
+    // one from memory acces violation
+    framework::master_test_suite().add( BOOST_TEST_CASE( &test_function ) );
+
+    return 0;
+}
+
+//____________________________________________________________________________//
+
+// EOF

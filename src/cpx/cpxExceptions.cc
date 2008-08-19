204e5719be8c9bf9bcd83eb1fadf58d4be6b167f
maronnax <maronnax@e2b6e7e4-f0f2-0310-8dec-eb45598a66c7>
2008-08-19 18:46:40 +0000

    Adding cpxExceptions.* files
    git-svn-id: https://moleculizer.svn.sourceforge.net/svnroot/moleculizer/branches/libmoleculizer@470 e2b6e7e4-f0f2-0310-8dec-eb45598a66c7

---
+++ src/cpx/cpxExceptions.cc
@@ -0,0 +1,40 @@
+/////////////////////////////////////////////////////////////////////////////
+// Moleculizer - a stochastic simulator for cellular chemistry.
+// Copyright (C) 2008  Molecular Sciences Institute
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//    
+// Contact information:
+//   Nathan Addy, Research Assistant     Voice: 510-981-8748
+//   The Molecular Sciences Institute    Email: addy@molsci.org  
+//   2168 Shattuck Ave.                  
+//   Berkeley, CA 94704
+/////////////////////////////////////////////////////////////////////////////
+
+
+#include "cpxExceptions.hh"
+
+namespace cpx
+{
+    std::string plexIsNotSimpleGraphXcpt::mkMsg()
+    {
+        std::ostringstream oss;
+        oss << "Error: Isomorphism checking on plexes require plexes be simple graphs "
+            << "(No intra-molecule bindings, at most one binding between different "
+            << "molecules).  This condition was not met.";
+        return oss.str();
+    }
+
+}

635ffe2b2d8644ee5e20b93e40f1d0f260648968
maronnax <maronnax@e2b6e7e4-f0f2-0310-8dec-eb45598a66c7>
2009-03-06 23:42:18 +0000

    Cleaning up a mess.
    git-svn-id: https://moleculizer.svn.sourceforge.net/svnroot/moleculizer/libmoleculizer/trunk@705 e2b6e7e4-f0f2-0310-8dec-eb45598a66c7

--- python-src/language_parser/setup.py
+++ python-src/language_parser/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name="moleculizer",
       version="1.0",
-      description "Moleculizer library for generating xml files from rules files.",
+      description = "Moleculizer library for generating xml files from rules files.",
       author = "Nathan Addy",
       author_email = "nathan.addy@gmail.com",
 
